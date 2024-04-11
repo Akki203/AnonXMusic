423e800d83493428b816965ec6669c961f40d8a4
Akki203 <143491334+Akki203@users.noreply.github.com>
2024-04-12 01:23:55 +0530

    Update config.py
    Signed-off-by: Akki203 <143491334+Akki203@users.noreply.github.com>

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Mahakal_203")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+DMf2IBBNzzEzMGM1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+ZMuKkv5Rdqk1NGU1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+ZMuKkv5Rdqk1NGU1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -78,13 +78,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/5b91fc72a1b94ac89c943.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/ab5927ee458ee430c70e2.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/ab5927ee458ee430c70e2.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
